7ee1365e1412830c321f0c2bb24619e0e796ae6c
samira220 <samira140206@gmail.com>
2024-05-23 13:47:13 +0300

    Добавьте файлы проекта.

---
+++ BlazorApp1/Students.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+
+namespace BlazorApp1
+{
+    public class Students
+    {
+        public Students(int id, string name, string surName, string lastName, string group, string photo, DateTime dateOfBirth)
+        {
+            Id = id;
+            Name = name;
+            SurName = surName;
+            LastName = lastName;
+            Group = group;
+            Photo = photo;
+            DateOfBirth = dateOfBirth;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set;}
+        public string LastName { get; set; }
+        public string Group { get; set; }
+        public string Photo { get; set; }
+        public  DateTime DateOfBirth { get; set; }
+
+    }
+}
